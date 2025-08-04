9dd17db351b23b5a0cd956fd02e515300e23b607
yasir5405 <yasirnaseem5415@gmail.com>
2025-08-04 22:23:35 +0530

    add creds to .env
    Changed .env files

--- src/utils/googleConfig.ts
+++ src/utils/googleConfig.ts
@@ -1,8 +1,5 @@
 import { google } from "googleapis";
 
-// console.log(process.env.GOOGLE_CLIENT_ID);
-// console.log(process.env.GOOGLE_CLIENT_SECRET);
-
 export const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
